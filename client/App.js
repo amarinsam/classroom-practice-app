596ed0ee69b1f30af84a76753f31859711c47a95
stevkim <stevenn.kkim@gmail.com>
2024-02-23 19:01:39 -0800

    First commit - boilterplate!

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column'}}>
+      <p>Good luck everyone! </p>
+      <p>Make sure to clear this with your own code :D</p>
+      <p>You guys got this!</p>
+    </div>
+  )
+}
+
+export default App;
